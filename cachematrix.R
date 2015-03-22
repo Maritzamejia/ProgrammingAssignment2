77b6d28047592d72c142320097440b063b6226fa
Maritza Mejia <maritza.mejia90@gmail.com>
2015-03-22 02:25:34 -0400

    solutions
    The solution codes for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## functions allows the matrix to store it's inverse value for 
+## faster computation times
+makeCacheMatrix <- function( m = matrix() ) {
+  
+  ## Initialize the inverse property
+  t <- NULL
+  set <- function( matrix ) {
+    m <<- matrix
+    t <<- NULL
+  }
+  ## setting up the matrix
+  get <- function() { m }
+  
+  ## setting the function to inverse the matrix
+  setInverse <- function(inverse) {
+    t <<- inverse
+  }
+  
+  ## getting the inverse of the matrix
+  getInverse <- function() { t }
+    ## Back the inverse property
+    
+  ## list of methods 
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Computing the inverse of the unique matrix back by "makeCacheMatrix"
+## Back to a matrix  "m"
+m <- x$getInverse()
+if( !is.null(m) ) {
+  message("getting stored data")
+  return(m)
 }
+## Compute the inverse via matrix multiplication
+m <- solve(data) %*% data
+## Setting the inverse to "x"
+x$setInverse(m)
+## show matrix
+m
+
+
+
+
